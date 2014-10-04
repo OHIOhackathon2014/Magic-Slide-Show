df23546628e964c60beb381b29a8dedfcbcf9034
Sophy Chen <sophychen2877@gmail.com>
2014-10-04 02:14:19 -0400

    audio functions (audio on and end)

--- speech.js
+++ speech.js
@@ -3,9 +3,142 @@ This is the speech api used to do the ditation and return the result.
 Maintainer:Sophy Chen
 */
 
-function MSS_startDic(){
-	
+var create_email=false;
+var share_media = false;
+var print = false;
+var final_transcript = '';
+var recognizing = false;
+var detecting = false;
+var ignore_onend;
+var start_timestamp;
+if (!('webkitSpeechRecognition' in window)) {
+  upgrade();
+} else {
+  start_button.style.display = 'inline-block';
+  var recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
+
+
+  recognition.onsoundstart = function() {
+    detecting = true;
+    showInfo('sound_detected');
+//    start_img.src = '/intl/en/chrome/assets/common/images/content/mic-animate.gif';
+  };
+  
+  recognition.onstart = function() {
+    if(detecting){
+    recognizing = true;
+    showInfo('info_speak_now');
+//    start_img.src = '/intl/en/chrome/assets/common/images/content/mic-animate.gif';
+    }
+  };
+  
+
+  recognition.onerror = function(event) {
+    if (event.error == 'no-speech') {
+      start_img.src = '/intl/en/chrome/assets/common/images/content/mic.gif';
+      showInfo('info_no_speech');
+      ignore_onend = true;
+    }
+    if (event.error == 'audio-capture') {
+//      start_img.src = '/intl/en/chrome/assets/common/images/content/mic.gif';
+      showInfo('info_no_microphone');
+      ignore_onend = true;
+    }
+    if (event.error == 'not-allowed') {
+      if (event.timeStamp - start_timestamp < 100) {
+        showInfo('info_blocked');
+      } else {
+        showInfo('info_denied');
+      }
+      ignore_onend = true;
+    }
+    if(event.error=='aborted'){
+      showinfo('speech_aborted');
+      ignore_onend=true;
+    }
+    if(event.error=='network'){
+    showinfo('network_falied');
+    ignore_onend=true;
+    }
+    
+  };
+  recognition.onsoundend=function(){
+     detecting=false;
+      if (ignore_onend) {
+      return;
+    }
+  
+  }
+  recognition.onend = function() {
+    if (!detecting){
+    recognizing = false;  
+//    start_img.src = '/intl/en/chrome/assets/common/images/content/mic.gif';
+    }
+    if (!final_transcript) {
+      showInfo('info_start');
+      return;
+    }
+    showInfo('');
+    if (window.getSelection) {
+      window.getSelection().removeAllRanges();
+      var range = document.createRange();
+      range.selectNode(document.getElementById('final_span'));
+      window.getSelection().addRange(range);
+    }
+    if (create_email) {
+      create_email = false;
+      createEmail();
+    }
+    if(share_media){
+      share_media=false;
+      share2Media();
+    }
+    if(print){
+      print=false;
+      printAsPdf();
+    }
+  };
+
+  recognition.onresult = function(event) {
+    var interim_transcript = '';
+    if (typeof(event.results) == 'undefined') {
+      recognition.onend = null;
+      recognition.stop();
+      upgrade();
+      return;
+    }
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+      }
+    }
+    final_transcript = capitalize(final_transcript);
+    final_span.innerHTML = linebreak(final_transcript);
+    interim_span.innerHTML = linebreak(interim_transcript);
+    if (final_transcript || interim_transcript) {
+      showButtons('inline-block');
+    }
+  };
 }
-function MSS_stopDic(){
-	
+
+function upgrade() {
+  start_button.style.visibility = 'hidden';
+  showInfo('info_upgrade');
 }
+
+var two_line = /\n\n/g;
+var one_line = /\n/g;
+function linebreak(s) {
+  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+
+var first_char = /\S/;
+function capitalize(s) {
+  return s.replace(first_char, function(m) { return m.toUpperCase(); });
+}
+
+
